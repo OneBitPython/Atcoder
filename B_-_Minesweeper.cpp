8e12fa6004e0dc7e4fc8640b9655521df1fc4f85
ananthram <ananthramtennis12@gmail.com>
2022-09-14 23:34:10 +0530

    committed for 14th september

---
+++ B_-_Minesweeper.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+const double PI=3.141592653589;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+
+
+void solve()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<vector<char>>a(n,vector<char>(m));
+    for(int i = 0;i<n;++i){
+        for(int j = 0;j<m;++j)cin >> a[i][j];
+    }
+    for(int i = 0;i<n;++i){
+        for(int j = 0;j<m;++j){
+            if(a[i][j] == '#'){
+                cout << a[i][j] ;
+                continue;
+            }
+            vector<pair<int,int>>neighbours;
+            if(i > 0)neighbours.pb({i-1,j});
+            if(i <n-1)neighbours.pb({i+1, j});
+            if(j > 0)neighbours.pb({i,j-1});
+            if(j < m-1)neighbours.pb({i,j+1});
+            if(i > 0 && j > 0)neighbours.pb({i-1,j-1});
+            if(i > 0 && j < m-1)neighbours.pb({i-1,j+1});
+            if(i < n-1 && j > 0)neighbours.pb({i+1, j-1});
+            if(i < n-1 && j <m-1)neighbours.pb({i+1,j+1});
+            int c = 0;
+            for(auto v : neighbours){
+                if(a[v.first][v.second]=='#')c++;
+            }
+            cout << c;
+        }
+        cout << endl;
+    }
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    // cin >> T;
+    for(int i = 1;i<=T;++i)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
